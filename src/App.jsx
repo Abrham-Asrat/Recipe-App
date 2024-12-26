4535e4beb1abaa92ce1a841b9f54004404957e90
abrham-asrat <abrhamasrat29@gmail.com>
2024-12-27 01:08:44 +0300

    💥First Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import Header from "./component/header"
-import Entry from "./component/entry"
-import './app.css'
+import Header from "./component/header";
+import Entry from "./component/entry";
+import "APP.css";
 export default function App() {
   return (
-     <>
-     <Header/>
-     <Entry/>
-     </>
-  )
+    <>
+      <Header />
+      <Entry />
+    </>
+  );
 }
